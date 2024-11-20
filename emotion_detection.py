832539505923c487668557d51255f4fc2ac64856
Alvin Shanmugam <cajs121@gmail.com>
2024-11-20 01:26:06 -0500

    Step 3 Complete

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,19 +3,12 @@ import requests, json
 def emotion_detector(text_to_analyse):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    json = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(URL, headers=Headers, json=json)
+    body = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(URL, headers=Headers, json=body)
     
-    data = response.json()['text']
-    dominant_emotion = max(data, key=data.get)
+    data = response.json()['emotionPredictions'][0]['emotion']
+    data['dominant_emotion'] = max(data, key=data.get)
 
-    return_dict = {
-        'anger': data['anger'],
-        'disgust': data['disgust'],
-        'fear': data['fear'],
-        'joy': data['joy'],
-        'sadness': data['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+    return data
 
-print(emotion_detector("I love this new technology."))
+print(emotion_detector("I am so happy I am doing this."))
